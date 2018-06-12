f71c9d7bd8f6f5d24b03665b48eadaf914526354
Patrick Hare <patrickhare42@gmail.com>
2018-06-12 14:59:13 +0100

    Updated api

--- utils/api.js
+++ utils/api.js
@@ -1,3 +1,17 @@
 import { AsyncStorage } from 'react-native';
 
 export const DECK_STORAGE_KEY = 'Flashcards'
+
+export const fetchDecks = () => {
+  return AsyncStorage.getItem(DECK_STORAGE_KEY).then(data => JSON.parse(data));
+}
+
+export const addDeck = deck =>
+  AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify(deck)).then(
+    data => deck
+  );
+
+export const addCard = deck =>
+  AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify(deck)).then(
+    data => deck
+  );
