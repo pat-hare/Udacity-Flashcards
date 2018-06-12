4f4bde5a71f400dadbffc88960b1ddf6d306f22a
Patrick Hare <patrickhare42@gmail.com>
2018-06-12 15:09:31 +0100

    Added Actions

--- utils/api.js
+++ utils/api.js
@@ -6,12 +6,12 @@ export const fetchDecks = () => {
   return AsyncStorage.getItem(DECK_STORAGE_KEY).then(data => JSON.parse(data));
 }
 
-export const addDeck = deck =>
+export const addNewDeck = deck =>
   AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify(deck)).then(
     data => deck
   );
 
-export const addCard = deck =>
+export const addDeckCard = deck =>
   AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify(deck)).then(
     data => deck
   );
